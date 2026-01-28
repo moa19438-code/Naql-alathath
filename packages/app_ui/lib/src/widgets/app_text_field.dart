0f5728c92fc5a72d08e1b5b1427b0e4f60bdefcf
moa19438-code <moa19438@gmail.com>
2026-01-28 21:07:24 +0300

    Create app_text_field.dart

---
+++ packages/app_ui/lib/src/widgets/app_text_field.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class AppTextField extends StatelessWidget {
+  final String? label;
+  final String? hint;
+  final TextEditingController? controller;
+  final TextInputType? keyboardType;
+  final bool obscureText;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final String? Function(String?)? validator;
+
+  const AppTextField({
+    super.key,
+    this.label,
+    this.hint,
+    this.controller,
+    this.keyboardType,
+    this.obscureText = false,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.validator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      keyboardType: keyboardType,
+      obscureText: obscureText,
+      validator: validator,
+      decoration: InputDecoration(
+        labelText: label,
+        hintText: hint,
+        prefixIcon: prefixIcon,
+        suffixIcon: suffixIcon,
+      ),
+    );
+  }
+}
