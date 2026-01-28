cc0705a9213e37f9158eba5d6a1d9cc838a7ed85
moa19438-code <moa19438@gmail.com>
2026-01-28 21:03:54 +0300

    Create app_ui.dart

---
+++ packages/app_ui/lib/app_ui.dart
@@ -0,0 +1,13 @@
+library app_ui;
+
+export 'src/theme/app_theme.dart';
+export 'src/theme/app_colors.dart';
+export 'src/theme/app_spacing.dart';
+export 'src/theme/app_radii.dart';
+export 'src/theme/app_typography.dart';
+
+export 'src/widgets/app_button.dart';
+export 'src/widgets/app_text_field.dart';
+export 'src/widgets/app_card.dart';
+export 'src/widgets/app_scaffold.dart';
+export 'src/widgets/app_skeleton.dart';
