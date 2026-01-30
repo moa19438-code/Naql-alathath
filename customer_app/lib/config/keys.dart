7483bfae9ddb6fe11e73c843314a0eae1ad8ea36
moa19438-code <moa19438@gmail.com>
2026-01-30 05:29:48 +0300

    Update keys.dart

--- customer_app/lib/config/keys.dart
+++ customer_app/lib/config/keys.dart
@@ -1,4 +1,4 @@
 /// ⚠️ المشروع خاص، لا ترفعه Public
 class AppKeys {
-  static const googleMapsApiKey = '‏AIzaSyBcZ1lyH1hUfV9MRvq-C2xhr4DlfJ5w49o';
+  static const googleMapsApiKey = 'AIzaSyBcZ1lyH1hUfV9MRvq-C2xhr4DlfJ5w49o';
 }
