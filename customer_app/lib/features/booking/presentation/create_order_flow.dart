98569537a6d15b19eb38a8281dd37ef202846daa
moa19438-code <moa19438@gmail.com>
2026-01-30 13:09:24 +0300

    Update create_order_flow.dart

--- customer_app/lib/features/booking/presentation/create_order_flow.dart
+++ customer_app/lib/features/booking/presentation/create_order_flow.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../../app/router.dart';
 import 'create_order_model.dart';
 import 'step_items_page.dart';
 import 'step_locations_page.dart';
@@ -38,14 +39,23 @@ class _CreateOrderFlowState extends State<CreateOrderFlow> {
 
   void _next() => setState(() => step++);
   void _back() => setState(() => step--);
-  void _close() => context.pop();
+
+  void _close() {
+    // ✅ إذا ما فيه صفحة قبلها (دخلت بـ go) ما راح يشتغل pop
+    if (context.canPop()) {
+      context.pop();
+    } else {
+      context.go(CustomerRoutes.home);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return switch (step) {
       0 => StepServicePage(
           selected: model.serviceType,
-          onSelect: (v) => setState(() => model = model.copyWith(serviceType: v)),
+          onSelect: (v) =>
+              setState(() => model = model.copyWith(serviceType: v)),
           onClose: _close,
           onNext: model.isStep1Valid ? _next : null,
         ),
@@ -75,7 +85,13 @@ class _CreateOrderFlowState extends State<CreateOrderFlow> {
           onBack: _back,
           onConfirm: (dt) {
             setState(() => model = model.copyWith(scheduledAt: dt));
-            context.pop(); // مؤقتًا
+
+            // ✅ مؤقتًا نرجع للهوم بدل pop (لأنه ممكن ما يشتغل مع go)
+            if (context.canPop()) {
+              context.pop();
+            } else {
+              context.go(CustomerRoutes.home);
+            }
           },
         ),
     };
